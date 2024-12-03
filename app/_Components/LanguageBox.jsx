ef44f9cecd6c029ef8b1995cd1890e3cc4cdc244
Sai Sashreek D <saisashreekd@gmail.com>
2024-12-03 10:48:10 +0530

    Language Selection for English Enabled

--- app/_Components/LanguageBox.jsx
+++ app/_Components/LanguageBox.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { useRouter } from "next/navigation";
 
 function LanguageBox() {
+  const navigate = useRouter();
+
+  const engLangHandler = (e) => {
+    e.preventDefault();
+    navigate.push("/eng/01");
+  };
+
   return (
     <>
       <div className="grid grid-cols-4 gap-4">
-        <button className="bg-[#e25a71] text-white py-2 px-4 rounded-lg shadow-lg hover:bg-[#c4122f] transition">
+        <button
+          className="bg-[#e25a71] text-white py-2 px-4 rounded-lg shadow-lg hover:bg-[#c4122f] transition"
+          onClick={engLangHandler}
+        >
           English
         </button>
         <button className="bg-[#e25a71] text-white py-2 px-4 rounded-lg shadow-lg hover:bg-[#c4122f] transition">
