3e5b5f75f42bfff2eef844b98755ff481d12acec
Sai Sashreek D <saisashreekd@gmail.com>
2024-12-24 15:53:19 +0530

    Build Issue Updated

--- app/(pages)/eng/09/page.jsx
+++ app/(pages)/eng/09/page.jsx
@@ -19,11 +19,25 @@ export default function Page9() {
       <section className="w-[100vw] h-[100vh] bg-cover page9 select-none">
         <div className="flex p-16 gap-10 text-2xl pr-80 text-justify">
           <div className="flex flex-col gap-4">
-            <p className="">Dadaji was expecting this question. He knew that Veer wanted to get into the root of the matter. He was prepared to answer. </p>
-            <p className="">Dadaji - For over fourteen hundred years, Cruel invaders attacked out land, ruled it, and caused great harm. They destroyed the famous universities of Nalanda, Takshashilla, and Vikramshilla, where many of our precious texts were kept. That is how we lost our ancient knowledge.</p>
+            <p className="">
+              Dadaji was expecting this question. He knew that Veer wanted to
+              get into the root of the matter. He was prepared to answer.{" "}
+            </p>
+            <p className="">
+              Dadaji - For over fourteen hundred years, Cruel invaders attacked
+              out land, ruled it, and caused great harm. They destroyed the
+              famous universities of Nalanda, Takshashilla, and Vikramshilla,
+              where many of our precious texts were kept. That is how we lost
+              our ancient knowledge.
+            </p>
           </div>
           <div className="pl-32">
-            <p className="">Just imagine! Veer, the great mathematician Aryabhatt who discovered 'Zero' was an esteemed teacher at Nalanda University. The Chinese scholar Hiuen Tsang studied at Nalanda University and even because a teacher there, so great was its name and fame.</p>
+            <p className="">
+              Just imagine! Veer, the great mathematician Aryabhatt who
+              discovered &apos;Zero&apos; was an esteemed teacher at Nalanda University.
+              The Chinese scholar Hiuen Tsang studied at Nalanda University and
+              even because a teacher there, so great was its name and fame.
+            </p>
           </div>
         </div>
       </section>
--- app/(pages)/eng/12/page.jsx
+++ app/(pages)/eng/12/page.jsx
@@ -17,18 +17,31 @@ export default function Page12() {
   return (
     <>
       <section className="w-[100vw] h-[100vh] bg-cover page12 select-none">
-      <div className="flex text-white w-full px-16 pt-20 gap-12 text-2xl ">
+        <div className="flex text-white w-full px-16 pt-20 gap-12 text-2xl ">
           <div className="text-justify flex pl-3 flex-col gap-4">
             <p className="w-[500px]">
-            Veer's mind wandered to something he suddenly remembered as Dadaji spoke. He interrupted him.
+              Veer&apos;s mind wandered to something he suddenly remembered as
+              Dadaji spoke. He interrupted him.
             </p>
-            <p>Veer - Dadaji, tell me who are 'Vikram' and 'Pragyan'? I heard something about them on T.V. Are they astronauts?</p>
+            <p>
+              Veer - Dadaji, tell me who are &apos;Vikram&apos; and
+              &apos;Pragyan&apos;? I heard something about them on T.V. Are they
+              astronauts?
+            </p>
           </div>
           <div className="text-justify flex pl-40 flex-col gap-4">
             <p>
-            Dadaji - 'Vikram' is the name of the lander that has landed on the moon. 'Pragyan' is the name of the rover that will explore and try to find answers to unexplored facts about the moon. The lander has been named 'Vikram' in honour of our great scientist Dr. Vikram Sarabhai. He was the founding father of India's space programme.
+              Dadaji - &apos;Vikram&apos; is the name of the lander that has
+              landed on the moon. &apos;Pragyan&apos; is the name of the rover
+              that will explore and try to find answers to unexplored facts
+              about the moon. The lander has been named &apos;Vikram&apos; in
+              honour of our great scientist Dr. Vikram Sarabhai. He was the
+              founding father of India&apos;s space programme.
             </p>
-            <p>Veer - I will surely read this book which tells about Vikram Sarabhai.</p>
+            <p>
+              Veer - I will surely read this book which tells about Vikram
+              Sarabhai.
+            </p>
           </div>
         </div>
       </section>
