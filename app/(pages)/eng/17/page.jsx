728a698d69b4bfc6808acf44b9506f8569f71675
Sai Sashreek D <saisashreekd@gmail.com>
2024-12-24 15:58:22 +0530

    Added &apos; in required space

--- app/(pages)/eng/17/page.jsx
+++ app/(pages)/eng/17/page.jsx
@@ -17,14 +17,24 @@ export default function Page17() {
   return (
     <>
       <section className="w-[100vw] h-[100vh] bg-cover page17 select-none">
-      <div className="flex pl-16 pt-10  gap-10 text-2xl pr-40 text-justify">
+        <div className="flex pl-16 pt-10  gap-10 text-2xl pr-40 text-justify">
           <div className="flex w-[200px] flex-col gap-4 ">
-              <p className="">Veer - What will this rover do on the moon? Nobody stays there. How will it help us? </p>
-              <p className="">Dadaji - Ha! Ha! Ha! You are wrong, little boy. Like Chandrayaan 1 gave valuable inputs about the possibility of water on the moon, this mission will explore the properties of the moon's surface, its soil, gases, metals and minerals.</p>
+            <p>
+              Veer - What will this rover do on the moon? Nobody stays there.
+              How will it help us?
+            </p>
+            <p>
+              Dadaji - Ha! Ha! Ha! You are wrong, little boy. Like Chandrayaan 1
+              gave valuable inputs about the possibility of water on the moon,
+              this mission will explore the properties of the moon&apos;s surface,
+              its soil, gases, metals and minerals.
+            </p>
           </div>
           <div className="flex flex-col gap-4">
-              <p className="">Veer smiled sheepishly. He wanted to show Dadaji that he was not a little boy. He knew a lot!</p>
-             
+            <p>
+              Veer smiled sheepishly. He wanted to show Dadaji that he was not a
+              little boy. He knew a lot!
+            </p>
           </div>
         </div>
       </section>
